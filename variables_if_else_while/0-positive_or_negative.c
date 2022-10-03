e2d1a79bac620641d95f9f0355a1445b64ec3f66
Salma5806 <5806@holbertonstudents.com>
2022-10-03 14:56:41 +0100

    tasks 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a  ;
+	printf("enter the number : ");
+	scanf("%d" , &a) ;
+	if (a>0)
+	printf("is positive")
+	else
+	printf("is negative")
+
+return (0);
+}
+
