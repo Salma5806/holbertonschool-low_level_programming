6db51925e9aced4b8e408a47d845df4c345d9af4
Salma5806 <5806@holbertonstudents.com>
2022-10-04 17:04:00 +0100

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -7,22 +7,15 @@
  */
 int main(void)
 {
-	char ch;
-	printf("Enter u to display uppercase alphabets.\n");
-	printf("Enter l to display lowercase alphabets.\n");
-	getchar("%ch , &ch)
+	char ch ;
 
-	if (ch == 'U' || ch == 'u')
-{
+	for (ch = 'A'; ch <= 'Z'; ch++)
+		putchar("%ch" , ch);
 
-		for (ch = 'A'; ch <= 'Z'; ++ch)
-			putchar("%ch" , ch);
-}
-	else if ( c == 'L' || c == 'l')
-{
-		for (ch == 'a'; c <= 'z'; ++ch)
-			putchar ("%ch" , c);
-}
+	for (ch == 'a'; c <= 'z'; ch++)
+		putchar ("%ch" , ch);
+		
+		putchar('\n');
 
 	return (0);
 }
