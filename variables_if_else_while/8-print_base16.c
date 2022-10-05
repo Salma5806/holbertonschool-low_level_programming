beb9a66e5f3231a23386544414f60ece53c5f7db
Salma5806 <5806@holbertonstudents.com>
2022-10-05 10:12:05 +0100

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -10,8 +10,8 @@ int main(void)
 	int i;
 	char ch;
 
-	for (i = 0 ; i <= 10 ; i++)
-		putchar((i % 10) + '0');
+	for (i = 0 ; i <= 9 ; i++)
+		putchar((i);
 
 	for (ch = 'a' ; ch <= 'f' ; ch++)
 		putchar(ch);
