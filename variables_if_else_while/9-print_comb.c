5c3e743d77745a705b2afbf9a27e02e63556615c
Salma5806 <5806@holbertonstudents.com>
2022-10-05 10:30:14 +0100

    combination

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,8 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-		putchar(%d,%d,%d, , i);
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+		if(i != '9')
+{
+			putchar(',');
+			putchar(' ');
+}
 
 	putchar("\n");
 
