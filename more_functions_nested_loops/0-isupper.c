5a034cc50fabc5c21028cf2380f2a1610c7b3a61
Salma5806 <5806@holbertonstudents.com>
2022-10-06 17:55:32 +0100

    uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * int_isupper - checks for uppercase character.
+ */
+int _isupper(int c)
+{
+	char ch;
+
+	if (ch >= 'A' && ch <= 'Z')
+		return (1);
+	else
+		return (0);
+
+}
