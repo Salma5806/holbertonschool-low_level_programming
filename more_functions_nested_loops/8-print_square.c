e3c340714f1069b676f09e45797ec006ed79bcce
Salma5806 <5806@holbertonstudents.com>
2022-10-06 21:28:05 +0100

    size

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_line - prints draws a straight line in the terminal.
+ * @n: the number to be printed.
+ */
+void print_square(int size)
+{
+	int size;
+
+	if (size > 0)
+	{
+		for (l = 0; l < size; l++)
+		{
+			for (d = 0; d < size; d++)
+				_putchar('#');
+
+			if (l == size - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
+
+	}
+
+	_putchar('\n');
+}
