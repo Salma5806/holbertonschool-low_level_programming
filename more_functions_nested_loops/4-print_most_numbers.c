2ecdb9aa2f203b49c2ba656c9117c2c380b26f55
Salma5806 <5806@holbertonstudents.com>
2022-10-06 19:52:52 +0100

    numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -2,12 +2,11 @@
 
 /**
  * print_most_numbers - prints the number,from 0 to 9.
- * @c: The character cheker.
- *
  */
 void print_most_numbers(void)
 {
 	int i;
+
 	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
