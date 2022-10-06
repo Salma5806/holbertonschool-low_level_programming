23f2c25d45ba7b678a13d9e6b174d62393cef16c
Salma5806 <5806@holbertonstudents.com>
2022-10-06 15:38:09 +0100

    battery street

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers.
+ *
+ */
+void print_to_98(int n)
+{
+	if (n = '0'; n <= 98; n++)
+		_putchar("%d, ", n--);
+
+		_putchar("%d\n", n);
+
+}
