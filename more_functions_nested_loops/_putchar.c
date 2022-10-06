6601484075661a40db863b48a2c7750c782fbbfd
Salma5806 <5806@holbertonstudents.com>
2022-10-06 17:41:48 +0100

    putchar

---
+++ more_functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
