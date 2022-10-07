72ba4301f5cf727b9dd3f98563ecdaea94baa221
Salma5806 <5806@holbertonstudents.com>
2022-10-07 09:10:16 +0100

    Fizz-Buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * prints the numbers from 1 100.
+ *//
+{
+	int i;
+
+{
+ 
+	for (i = 1; i <= 100; i++)
+
+	{
+		if ((i % 3) == 0 ) 
+			printf("Fizz");
+
+		else if ((i % 5) == 0)
+			printf("Buzz");
+
+		else (((i % 3) == 0) && (i % 5) == 0))
+			printf("Buzz");
+
+	}
+	printf("\n");
+}
+
