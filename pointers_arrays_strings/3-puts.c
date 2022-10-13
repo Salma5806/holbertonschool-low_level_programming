c3c7c9692774d58a609bdcfe10657942eb9f357a
Salma5806 <5806@holbertonstudents.com>
2022-10-13 17:56:57 +0100

    prints

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * void _puts - prints a string.
+ * @str: the string.
+ */
+void _puts(char *str);
+{
+	int index = 0;
+	while(*str != '\0')
+	{
+	_putchar("%c", *str[index]);
+	index++
+	}
+}
