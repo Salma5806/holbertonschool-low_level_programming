1d9d12309116a2b1d1ff2aa2a100b62976c6ffe8
Salma5806 <5806@holbertonstudents.com>
2022-10-14 18:50:27 +0100

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,21 +9,13 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, s1 = 0, s2 = 0;
+	int b, s1 = 0, s2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (i = 0; b < size; i++)
 	{
-		s1 += a[i];
-		a += size;
-	}
-
-	a -= size;
-
-	for (i = 0; i < size; i++)
-	{
-		s2 += a[i];
-		a -= size;
+		s1 += a[(size + 1) * b];
+		s2 += a[(size - 1) * (b + 1)];
 	}
 
 	printf("%d, %d\n", s1, s2);
-
+}
