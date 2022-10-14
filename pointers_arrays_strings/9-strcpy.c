d661bcd2273c5d96e92ba65abe5c32fae31d5094
Salma5806 <5806@holbertonstudents.com>
2022-10-14 12:20:08 +0100

    copies string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -4,16 +4,17 @@
  * char *_strcpy - copies the string pointed.
  * @dest: parameter to the buffer pointed.
  * @src: string pointed.
+ * Return: a pointer to the destination string @dest.
  */
 char *_strcpy(char *dest, char *src)
 {
 	int c = 0;
+
 	while (dest[c] != '\0')
+	{
 		c++;
 		dest++;
+	}
 
-	printf("%c", dest[c]);
-	printf("\n");
-	printf("%c", dest[c]);
-
+	return dest;
 }
