0c574d0fd9350b3d076dfeaadaa93c8f15b4370e
Salma5806 <5806@holbertonstudents.com>
2022-10-14 15:01:41 +0100

    compared

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _strcmp - compares pointers to two strings .
+* @s1: a pointer to the first string to be compared.
+* @s2: a pointer to the second string to be compared.
+*
+*Return: the result of comparaison.
+*/
+int _strcmp(char *s1, char *s2)
+{
+	int s1 = 0, s2 = 0;
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return (*s1);
+	return (*s2);
+}
