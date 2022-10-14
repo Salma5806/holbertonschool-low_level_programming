3223ac157f8decccdd6a0a9080e514f7f7f39429
Salma5806 <5806@holbertonstudents.com>
2022-10-14 14:52:46 +0100

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,17 +4,19 @@
  * @n: is the pointer is the origin.
  * @dest: is the pointer is the destiny.
  * @src: is the pointer is the destiny.
- * Return: d.
+ * Return: string.
  */
 
-char *_strncpy(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int  n)
 {
-	int i;
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-		dest[i] = '\0';
-	}
+	int i = 0, src_len = 0;
 
+	while (src[i++])
+		src_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
 	return (dest);
 }
