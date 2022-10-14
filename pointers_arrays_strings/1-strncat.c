ad89e7bf3cbd6d0cedd739cff131bc293a58ccf3
Salma5806 <5806@holbertonstudents.com>
2022-10-14 17:06:42 +0100

    strncat

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,6 +9,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
+	int i = 0, dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i] && i < n; i++)
+		dest[dest_len++] = src[i];
+
 	return (dest);
 }
