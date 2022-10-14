5be4ebb3e6828bb63b8b23b647bdfe8deb536059
Salma5806 <5806@holbertonstudents.com>
2022-10-14 11:40:25 +0100

    arrays

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_array - Prints arrays.
  * @a: parameter pointer.
@@ -11,10 +12,11 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-	printf("%d\n", a[i]);
+		printf("%d", a[i]);
 
-	if (i < (n - 1))
-		printf(", ");
+		if (i < (n - 1))
+			printf(", ");
 	}
 
+	printf("\n");
 }
