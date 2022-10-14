051f1354c565b2b4291a317c4c1b38c4cc038593
Salma5806 <5806@holbertonstudents.com>
2022-10-14 14:05:11 +0100

    appends

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * char *_strncat - appends the src string to the dest string.
+ * @dest: parameter to the buffer pointed.
+ * @src: string pointed.
+ * Return: a pointer to the destination string @dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(*dest, *src, n)
+	return (dest);
+}
