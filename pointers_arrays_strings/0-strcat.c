ee2551ec55fc8479afcd123f1eedce12cdd9cab3
Salma5806 <5806@holbertonstudents.com>
2022-10-14 13:24:46 +0100

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * char *_strcat - appends the src string to the dest string.
+ * @dest: destination of string.
+ * @src: source of string.
+ * Return:a pointer to the destination string @dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, len1, len2;
+
+	len1 = strlen(dest);
+	len2 = strlen(src);
+
+	for (i = 0; i < len2; i++)
+	{
+		dest[len1 + i] = src[i];
+	printf("%s", dest);
+	}
+	return dest;
+
+}
