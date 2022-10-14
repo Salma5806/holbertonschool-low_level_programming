20df6af6f0f49053cbdd9c9d593b453e380559b4
Salma5806 <5806@holbertonstudents.com>
2022-10-14 19:05:01 +0100

    strchr

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,14 @@
 */
 char *_strchr(char *s, char c)
 {
-	int a = 0, b;
 
-	while (s[a])
-		a++;
-	for (b = 0; b < a; b++)
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
 	{
-		if (s[b] == c)
-			s += b;
-		return (s);
+		if (s[index] == c)
+			return (s + index);
 	}
+
 	return ('\0');
 }
