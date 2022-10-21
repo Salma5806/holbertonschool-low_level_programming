a6bb21696ea1cfdd4959b16cb7c43fb829249b1c
Salma5806 <5806@holbertonstudents.com>
2022-10-21 08:59:55 +0100

    print name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -6,8 +6,8 @@
 * @argv: an array of a command listed
 * Return: 0 for success
 */
-int main (int argc, char *argv[])
+int main (int _attribute_((_unused_)) argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	printf("%s\n", *argv[0]);
 		return (0);
 }
