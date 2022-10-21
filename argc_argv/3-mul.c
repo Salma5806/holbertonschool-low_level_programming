edd1ef94e11877adc026d323b2c32445c0210146
Salma5806 <5806@holbertonstudents.com>
2022-10-21 12:58:54 +0100

    multiplies two numbers

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -4,11 +4,12 @@
 /**
 * main - this is  a function to print its name
 * @argc: argc parameter
-* @argv: an array of a command listed 
+* @argv: an array of a command listed.
 * Return : 0 for success
 */
 int main(int argc, char *argv[])
 {
+
 	int result = 0, num1, num2;
 
 	if (argc != 3)
