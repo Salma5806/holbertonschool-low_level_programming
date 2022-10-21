26aa765c158a73c969b57b79bf2c0feb790d442e
Salma5806 <5806@holbertonstudents.com>
2022-10-21 09:18:19 +0100

    print numbers

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* main - this is a function to print its name.
+* @argc: argc parameter.
+* @argv: an array of a command listed.
+*/
+int main(int argc, char *argv[] _attribute_((unuses))
+{
+	printf("%d\n", argc - 1);
+		return (0);
+}
