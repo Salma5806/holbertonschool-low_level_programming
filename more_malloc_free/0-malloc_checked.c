31c57a5d85d2bf33f3caebd7a51a0da80b179ba4
Salma5806 <5806@holbertonstudents.com>
2022-10-28 16:58:50 +0100

    malloc checked

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * malloc_checked - Entry Point
+ * @b: parameter
+ * Return: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *v;
+
+	v = malloc(size of (int) * b);
+
+	if (n == NULL)
+		exit(98);
+	return (n);
+}
