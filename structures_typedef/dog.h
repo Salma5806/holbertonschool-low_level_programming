63aaf87312dc1df16fb2679470c8e4d6bb0c5f2a
Salma5806 <5806@holbertonstudents.com>
2022-11-02 17:02:40 +0100

    structure dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 
-/*
+/**
 * struct dog - a new type describing a dog.
 * @name: the name of the dog
 * @age: the age of the dog
@@ -14,3 +14,14 @@ struct dog
 		float age;
 		char *owner;
 	};
+/**
+* dogPoopy - typedef for struct dog
+*/
+
+typedef struct dog dogPoopy;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
