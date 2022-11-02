db23ad212459602fd957c65707bd34adff4a1e81
Salma5806 <5806@holbertonstudents.com>
2022-11-02 18:13:37 +0100

    intialization

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,13 +2,17 @@
 #include <stdio.h>
 
 /*
-*
-*
+* int_dog - intialize a variable of type struct dog
+* @d: struct dog
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	int main (void)
 
-	dog_t *d = {*name, age, *owner}
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
