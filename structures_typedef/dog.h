99075960578cf48d1e7662f6df2a0fbcb7ccfc02
Salma5806 <5806@holbertonstudents.com>
2022-11-02 16:52:12 +0100

    structure dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef DOG_H
+#define DOG_H
+//*
+* struct dog - a new type describing a dog.
+* @name: the name of the dog
+* @age: the age of the dog
+* @owner: the owner of the dog
+*/
 
 	struct dog
 	{
