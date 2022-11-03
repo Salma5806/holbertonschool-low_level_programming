dc04cd88106b3e87c570908b32b11e4833979191
Salma5806 <5806@holbertonstudents.com>
2022-11-03 19:40:14 +0100

    poppy

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,23 +6,19 @@
 * @name: the name of the dog
 * @age: the age of the dog
 * @owner: the owner of the dog
+* Description: this struct is for dog
 */
 
 struct dog
-	{
-		char *name;
-		float age;
-		char *owner;
-	};
-/**
-* dog_t - Typedef for struct dog
-*/
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
