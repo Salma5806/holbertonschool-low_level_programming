4cf1bf6b4fc617edef0db9d29a37d141c1e4608e
Salma5806 <5806@holbertonstudents.com>
2022-11-04 20:26:49 +0100

    print name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,45 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/*
+*
+*
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+    {
+        f(name);
+    }
+}
+
+void print_name_uppercase(char *name)
+{
+	unsigned int i = 0;
+
+	while (name[i])
+	{
+		if (name[i] >= 'a' && name[i] <= 'z')
+		{
+			putchar(name[i] + 'A' - 'a');
+		}
+		else
+		{
+			putchar(name[i]);
+		}
+	i++;
+	}
+}
+void print_name_is(char *name)
+{
+	printf("%s", name);
+}
+
+int main()
+{
+	print_name(print_name_is);
+	print_name(print_name_uppercase);
+	printf("\n");
+
+	return 0;
+}
