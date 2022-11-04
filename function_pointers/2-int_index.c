f47c2905279f052ecaddee74adb0fac45878f643
Salma5806 <5806@holbertonstudents.com>
2022-11-04 22:28:37 +0100

    index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -3,15 +3,15 @@
 /*
 * int_index - searches for an integer
 * @array: array to search in
-* @size: size of the array 
+* @size: size of the array
 * @cmp : pointer to the comparing function
-* return : index of the first element for which the cmp function does not return 0 or -1 if no match is found or size is negative 
+* return : index of the first element for which the cmp function does not return 0 or -1 if no match is found or size is negative
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL && cmp == NULL)
+	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
