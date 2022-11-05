4e18f8e3a382bb27b87c62c6bae63a08e87b5918
Salma5806 <5806@holbertonstudents.com>
2022-11-05 21:22:16 +0100

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -37,47 +37,7 @@ char *_strncpy(char *dest, char *src, int  n)
 		src_len++;
 
 	for (i = 0; src[i] && i < n; i++)
-		dest[i] = src[i];
-#include "dog.h"
-#include <stdlib.h>
-
-int _strlen(char *str);
-char *_strcopy(char *dest, char *src);
-dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * _strlen - Finds the length of a string.
- * @str: The string to be measured.
- *
- * Return: The length of the string.
- */
-int _strlen(char *str)
-{
-	int len = 0;
-
-	while (*str++)
-		len++;
-
-	return (len);
-}
-
-/**
- * _strcopy - Copies a string pointed to by src, including the
- *            terminating null byte, to a buffer pointed to by dest.
- * @dest: The buffer storing the string copy.
- * @src: The source string.
- *
- * Return: The pointer to dest.
- */
-char *_strcopy(char *dest, char *src)
-{
-	int index = 0;
-
-	for (index = 0; src[index]; index++)
-		dest[index] = src[index];
-
-	dest[index] = '\0';
-
+		dest[i] = src[i]
 	return (dest);
 }
 
