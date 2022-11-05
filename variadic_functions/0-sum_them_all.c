b9261f8ba2a75f25c8be8f522d16658da3cf1fd4
Salma5806 <5806@holbertonstudents.com>
2022-11-05 19:03:30 +0100

    sum numbers

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/*
+* sum_them_all : returns the sum of all the numbers
+* @n : amount of the argument
+* Return: sum of all numbers
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list k;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(k, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(k, int);
+	va_end(k);
+
+	return sum;
+}
