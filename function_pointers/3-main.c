88e9f03535f5811a10f4170a7a5d3b61b1efd5d6
Salma5806 <5806@holbertonstudents.com>
2022-11-05 23:27:55 +0100

    main

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -9,7 +9,7 @@
  *
  * Return: Always 0.
  */
-int main(int __attribute__((__unused_)) argc, char *argv[])
+int main(int __attribute__((__unused_)) argc, char * argv[])
 {
 	int num1, num2;
 	char *op;
