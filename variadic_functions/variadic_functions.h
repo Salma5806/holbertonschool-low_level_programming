18a6a538ca28ad5b2329874d82cd0210b401e508
Salma5806 <5806@holbertonstudents.com>
2022-11-05 18:42:13 +0100

    prototype

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+#include <stdlib.h>
+
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
