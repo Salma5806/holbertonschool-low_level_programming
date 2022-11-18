8822400778d9bc59c4a26dbdc5295f70c038edbf
Salma5806 <5806@holbertonstudents.com>
2022-11-18 12:52:28 +0100

    number of element

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/*
+*vlist_len - finds the number of elements in a linked list_t list
+* @h: the linked list_t list.
+*
+* Return: the number of element in h
+*/
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != 0)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
