5aaa922afb368063c414503faa4a7853954b0b3b
Salma5806 <5806@holbertonstudents.com>
2022-11-19 16:29:23 +0100

    sum list

---
+++ more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/*
+* sum_listint - calculates the sum of all the data (n) of a listint_t list
+* @head: a pointer to the head of the listint_t list
+*
+* Return: if the list is empty
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
