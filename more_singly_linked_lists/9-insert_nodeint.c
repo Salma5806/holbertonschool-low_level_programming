036db587cb8090496e921b3eb628cc23ed5ad185
Salma5806 <5806@holbertonstudents.com>
2022-11-19 17:33:27 +0100

    insert

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (newnode == NULL)
 		return (NULL);
 
-	newnode->newnode = newnode;
+	newnode->n = n;
 
 	if (idx == 0)
 	{
