a20c9f9dddcc90c97fe03841f0aa7ade50355eed
Salma5806 <5806@holbertonstudents.com>
2022-11-19 19:49:18 +0100

    flipbits

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+  * flip_bits -  counts the number of bits
+  * @n: number
+  * @m: number to flip n to
+  *
+  * Return:  the necessary number of bits to flip to get from n to m
+  */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int xor = 0, count = 0;
+
+	xor = n ^ m;
+	while (xor)
+	{
+		if (xor & 1)
+			count++;
+
+		xor >>= 1;
+	}
+
+	return (count);
+}
