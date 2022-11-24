6f1a175499e22de12ae67ac2722a15ad900ddf08
Salma5806 <5806@holbertonstudents.com>
2022-11-24 19:48:56 +0100

    prototype

--- file_io/main.h
+++ file_io/main.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+
 int _putchar(char);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
