1b230afca8501e486ab9358b5b2db95b59113dd4
Salma5806 <5806@holbertonstudents.com>
2022-11-24 20:51:28 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
 /**
 * create_file - creates a file
 * @filename : the source of file.
 * @text_content: content writed in the file
+*
 * Return : 1 it it success, -1 if it fails
 */
 int create_file(const char *filename, char *text_content)
@@ -14,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0600);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
