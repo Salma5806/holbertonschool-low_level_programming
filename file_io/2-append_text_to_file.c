ca02baece3de167456608048dd9fa7b0caf44525
Salma5806 <5806@holbertonstudents.com>
2022-11-24 21:20:11 +0100

    append text

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -20,7 +20,6 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (i = 0; text_content[i]; i++);
-
 		rwr = write(fd, text_content, i);
 		if (rwr == -1)
 			return (-1);
