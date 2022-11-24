55e93128af917e76ecbcbb64030ff2356fbbec3d
Salma5806 <5806@holbertonstudents.com>
2022-11-24 20:52:48 +0100

    create file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,8 +24,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (i = 0; text_content[i]; i++);
-
-	rwr = write(fd, text_content, i);
+		rwr = write(fd, text_content, i);
 	if (rwr == -1)
 		return (-1);
 	close(fd);
