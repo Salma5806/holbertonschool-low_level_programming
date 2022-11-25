3d87411113d4fc60d25104cb6cf10aba2360d7c6
Salma5806 <5806@holbertonstudents.com>
2022-11-25 21:32:44 +0100

    index

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include "lists.h"
 /*
-* get_nodeint_at_index - locates a given node of a listint_t linked list.
+* get_dnodeint_at_index - locates a given node of a listint_t linked list.
 * @head: A pointer to the head of the listint_t list
 * @index: the index of the node to locate - indices start at 0
 *
