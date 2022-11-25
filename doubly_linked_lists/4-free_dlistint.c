8dd9e31c9d0cb41ce9eedf64faa897b43b30bf13
Salma5806 <5806@holbertonstudents.com>
2022-11-25 21:14:26 +0100

    free dlistint

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+* free_dlistint - frees double linked list
+* @head: the pointer to the first node
+*
+*
+*/
+void free_dlistint(dlistint_t *head)
+{
+	ist_t *newnode;
+
+	while (head != NULL)
+	{
+		newnode = head;
+		head = head->next;
+		free(newnode->n);
+		free(newnode);
+	}
+	free(head);
+}
