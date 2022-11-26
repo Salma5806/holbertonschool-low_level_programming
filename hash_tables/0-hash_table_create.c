680f6eafc2a844acddae7d6d4860ac1942f147e2
Salma5806 <5806@holbertonstudents.com>
2022-11-26 15:37:51 +0100

    create hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -3,13 +3,14 @@
 #include "hash_tables.h"
 
 /**
-  * hash_table_create - Creates a hash table
-  * @size: Size of the array
-  *
-  * Return: A pointer to the newly created hash table
-  */
+* hash_table_create - Creates a hash table
+* @size: Size of the array
+*
+* Return: A pointer to the newly created hash table
+*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
+
 	unsigned long int i = 0;
 	hash_table_t *htble = NULL;
 	hash_node_t **arr = NULL;
