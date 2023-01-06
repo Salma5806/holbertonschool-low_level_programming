ba7640ee5956d011884c91598875210842202e35
Salma5806 <5806@holbertonstudents.com>
2023-01-06 18:45:21 +0100

    prototype

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef HOLBERTON_H_INCLUDED
+#define HOLBERTON_H_INCLUDED
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
+
