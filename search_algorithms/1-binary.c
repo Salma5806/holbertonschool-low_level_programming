ce1e6d171f7260b02edb23bd0321a5f25d720a1d
Salma5806 <5806@holbertonstudents.com>
2023-01-06 21:19:40 +0100

    binary

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+/**
+*
+*
+*
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	size_t start_index = 0, i = 0;
+	size_t end_index = size - 1;
+	size_t mid_index = 0;
+
+	if (array == NULL)
+		return (-1);
+	while (start_index <= end_index)
+	{
+		mid_index = start_index + (size / 2);
+			i = start_index;
+
+		printf("Searching in array:");
+
+		for (; i <= end_index; ++i)
+		{
+			if (i != start_index)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		printf("\n");
+
+		if (array[mid_index] > value)
+		{
+			end_index = mid_index - 1;
+		}
+		else
+		{
+			start_index = mid_index + 1;
+		}
+	}
+	return -1;
+}
