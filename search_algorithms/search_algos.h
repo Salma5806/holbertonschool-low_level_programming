fa5916e4560a407d6f2124d917da2c4151413b7b
Salma5806 <5806@holbertonstudents.com>
2023-01-06 22:30:41 +0100

    search

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H_INCLUDED
 #define HOLBERTON_H_INCLUDED
 #include <stdlib.h>
-
+int jump_search(int *array, size_t size, int value);
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 #endif
